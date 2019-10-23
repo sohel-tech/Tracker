96cc2e76ab620bd70260195909d555faa1e29b60
Sohel Mohammed <sohelpashamohammed@gmail.com>
2019-10-23 10:53:38 -0400

    Added Context and Provider and Spacer for good Design

--- tracks/App.js
+++ tracks/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function App() {
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+import { Provider as AuthProvider } from './src/context/AuthContext;'
+
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      trackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+    CreateTrack: TrackCreateScreen,
+    Account: AccountScreen
+  })
+});
+
+const App = createAppContainer(switchNavigator);
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
-        <View>
-            <Text> Signup  Screen</Text>
+        <View style={styles.container}>
+            <Spacer>
+                <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input label="Email" value={email} onChangeText={setEmail} autoCorrect={false} autoCapitalize="none"></Input>
+            <Spacer />
+            <Input secureTextEntry label="Password" value={password} onChangeText={setPassword} autoCapitalize="none" autoCorrect={false} ></Input>
+            <Spacer>
+                <Button title="Sign Up" />
+            </Spacer>
         </View>
     );
 
 };
 
-const styles = StyleSheet.create({
+SignupScreen.navigationOptions = () => {
+    return {
+        header: null
+    };
+};
 
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 10,
+        flex: 1,
+        justifyContent: 'center'
+    }
 });
 
 export default SignupScreen;

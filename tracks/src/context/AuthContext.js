e0afd3b3c6c6b2a74924b2474ece40091e842965
Sohel Mohammed <sohelpashamohammed@gmail.com>
2019-10-24 11:16:49 -0400

    signout added

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -12,6 +12,8 @@ const authReducer = (state, actions) => {
             return { errorMessage: '', token: actions.payload };
         case 'clear_error_message':
             return { ...state, errorMessage: '' };
+        case 'signout':
+            return { token: null, errorMessage: '' };
         default:
             return state;
     }
@@ -61,10 +63,10 @@ const signin = (dispatch) => async ({ email, password }) => {
     }
 };
 
-const signout = (dispatch) => {
-    return () => {
-
-    };
+const signout = (dispatch) => async () => {
+    await AsyncStorage.removeItem('token');
+    dispatch({ type: 'signout' });
+    navigate('loginFlow');
 };
 
 export const { Provider, Context } = createDataContext(

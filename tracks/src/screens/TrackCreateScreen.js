b77377bf82e31552703ad07fbd66ca3a6ab9648a
Sohel Mohammed <sohelpashamohammed@gmail.com>
2019-10-27 00:50:13 -0400

    TrackForm Added to TrackCreateScreen

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -6,6 +6,7 @@ import Map from '../components/Map';
 import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
+import TrackForm from '../components/TrackForm';
 
 
 const TrackCreateScreen = ({ isFocused }) => {
@@ -19,6 +20,7 @@ const TrackCreateScreen = ({ isFocused }) => {
             <Map />
 
             {err ? <Text>Please enable location services</Text> : null}
+            <TrackForm />
         </SafeAreaView>
     );
 
